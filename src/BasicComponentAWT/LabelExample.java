4a2766974593cf339140bdd538f3514cbfcd8069
Leah Deshar <leah.desar@gmail.com>
2024-06-06 14:03:56 +0545

    AWT Frame and label example

--- src/BasicComponentAWT/LabelExample.java
+++ src/BasicComponentAWT/LabelExample.java
@@ -1,4 +1,32 @@
 package BasicComponentAWT;
 
-public class LabelExample {
+import java.awt.*;
+import java.awt.event.*;
+
+public class LabelExample extends Frame {
+
+    public LabelExample() {
+        // Set the frame properties
+        setTitle("AWT Label Example");
+        setSize(300, 200);
+        setLayout(new FlowLayout());
+        setVisible(true);
+
+        // Create a label and add it to the frame
+        Label label = new Label("This is an AWT Label");
+        add(label);
+
+        // Add a window listener to handle window closing event
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the LabelExample class
+        new LabelExample();
+    }
 }
